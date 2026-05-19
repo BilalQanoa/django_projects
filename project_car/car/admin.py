c09b1726424196c92cb81e0ef35a638f9318f03c
BilalQanoa <bilalqanoa@gmail.com>
2026-05-19 14:46:36 +0300

    edits on  admin dashboard

--- project_car/car/admin.py
+++ project_car/car/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from car.models import Car
 
-# Register your models here.
+admin.site.site_header = "CAR DASHBOARD"
+
+class CarModel(admin.ModelAdmin):
+    list_display = ['id','name', 'year']
+    list_filter = ['name']
+    search_fields = ['name']
+    exclude = ['year']
+
+admin.site.register(Car, CarModel)
+
